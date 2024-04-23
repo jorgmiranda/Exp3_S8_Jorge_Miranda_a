4cbb3e8a9cf98ca7dd35b2c3cb53bbf303dc7fee
jorge-miranda <jorge.miranda@mysolutions.cl>
2024-04-23 15:09:50 -0400

    Se implementan las pruebas en Junit para la evaluación y se quita liberia adicional

--- src/main/java/com/sumativa/a/usuario/controller/RolController.java
+++ src/main/java/com/sumativa/a/usuario/controller/RolController.java
@@ -3,7 +3,6 @@ package com.sumativa.a.usuario.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
